46b1d22c5252242fb394abea80a7bca1bf85744c
Ajnas N B <114915484+AjnasNB@users.noreply.github.com>
2023-08-13 23:09:32 +0530

    Create q1.py
    Answer of q1

---
+++ Q1/q1.py
@@ -0,0 +1,11 @@
+from typing import *
+
+def dataTypes(type: str):
+    data_types = {
+        "Integer": 4,
+        "Long": 8,
+        "Float": 4,
+        "Double": 8,
+        "Character": 1
+    }
+    return data_types[type]
